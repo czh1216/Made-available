395667a600a0c3a86f44c7f9d98e817d09db260e
czh <2593620983@qq.com>
2022-08-02 09:15:46 +0800

    页面结构创建

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,92 +1,138 @@
 <template>
   <div class="navbar">
-    <div class="navbar-login">
-      <img src="@/assets/imgs/logo1.png" />
-    </div>
-    <div class="navbar-user">
-      <div class="navbar-userImg">
-        <img src="@/assets/imgs/userImg.png" />
-      </div>
-      <span>欢迎您, admin</span>
-      <el-dropdown
-        class="avatar-container"
-        trigger="click"
-        @click.native="logout"
-      >
+    <hamburger :is-active="sidebar.opened" class="hamburger-container" @toggleClick="toggleSideBar" />
+
+    <breadcrumb class="breadcrumb-container" />
+
+    <div class="right-menu">
+      <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          退出
+          <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
+        <el-dropdown-menu slot="dropdown" class="user-dropdown">
+          <router-link to="/">
+            <el-dropdown-item>
+              Home
+            </el-dropdown-item>
+          </router-link>
+          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
+            <el-dropdown-item>Github</el-dropdown-item>
+          </a>
+          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
+            <el-dropdown-item>Docs</el-dropdown-item>
+          </a>
+          <el-dropdown-item divided @click.native="logout">
+            <span style="display:block;">Log Out</span>
+          </el-dropdown-item>
+        </el-dropdown-menu>
       </el-dropdown>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex'
+import Breadcrumb from '@/components/Breadcrumb'
+import Hamburger from '@/components/Hamburger'
 
 export default {
   components: {
+    Breadcrumb,
+    Hamburger
   },
   computed: {
-    ...mapGetters(["sidebar", "avatar"]),
+    ...mapGetters([
+      'sidebar',
+      'avatar'
+    ])
   },
   methods: {
     toggleSideBar() {
-      this.$store.dispatch("app/toggleSideBar");
+      this.$store.dispatch('app/toggleSideBar')
     },
     async logout() {
-      await this.$store.dispatch("user/logout");
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`);
-    },
-  },
-};
+      await this.$store.dispatch('user/logout')
+      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .navbar {
-  display: flex;
-  justify-content: space-between;
-  position: fixed;
-  top: 0;
-  z-index: 99999;
-  height: 60px;
-    width: 100%;
-  // background-color: rgb(207, 37, 37);
-  background: url(~@/assets/imgs/banner.png) no-repeat;
-  padding-right: 50px;
-  background-size: 100%;
-  .navbar-login {
-    width: 88px;
-    height: 36px;
-    margin-top: 10px;
-    margin-left: 15px;
-    img {
-      width: 100%;
-      height: 100%;
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0,21,41,.08);
+
+  .hamburger-container {
+    line-height: 46px;
+    height: 100%;
+    float: left;
+    cursor: pointer;
+    transition: background .3s;
+    -webkit-tap-highlight-color:transparent;
+
+    &:hover {
+      background: rgba(0, 0, 0, .025)
     }
   }
-}
-.navbar-user {
-  display: flex;
-  align-items: center;
-  width: 200px;
-  .navbar-userImg {
-    margin-right: 20px;
+
+  .breadcrumb-container {
+    float: left;
   }
-  span {
-    white-space: nowrap;
-    color: #fff;
-    font-size: 16px;
-    margin-right: 20px;
-  }
-  .avatar-container {
-    margin-right: 20px;
-    color: #fff;
-    font-size: 16px;
-    cursor: pointer;
-    .avatar-wrapper {
-      white-space: nowrap;
+
+  .right-menu {
+    float: right;
+    height: 100%;
+    line-height: 50px;
+
+    &:focus {
+      outline: none;
+    }
+
+    .right-menu-item {
+      display: inline-block;
+      padding: 0 8px;
+      height: 100%;
+      font-size: 18px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
+
+      &.hover-effect {
+        cursor: pointer;
+        transition: background .3s;
+
+        &:hover {
+          background: rgba(0, 0, 0, .025)
+        }
+      }
+    }
+
+    .avatar-container {
+      margin-right: 30px;
+
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+
+        .user-avatar {
+          cursor: pointer;
+          width: 40px;
+          height: 40px;
+          border-radius: 10px;
+        }
+
+        .el-icon-caret-bottom {
+          cursor: pointer;
+          position: absolute;
+          right: -20px;
+          top: 25px;
+          font-size: 12px;
+        }
+      }
     }
   }
 }
